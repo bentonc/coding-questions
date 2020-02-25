f43df37ab8dc51d48b1510fe497cd4ef6039b5c9
Charles Benton <bentonc@gmail.com>
2020-02-24 22:54:54 -0800

    Add problem statement for apply permutation problem

--- CodingQuestions/src/org/bentonc/codingquestions/elementsofprogramminginterviews/PermuteArray.java
+++ CodingQuestions/src/org/bentonc/codingquestions/elementsofprogramminginterviews/PermuteArray.java
@@ -7,6 +7,17 @@ import org.bentonc.codingquestions.utils.LogUtils;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * A permutation is a rearrangement of members of a sequence into a new sequence. For example, there are 24
+ * permutations of <a, b, c, d>; some of these are <b, a, d, c>, <d, a, b, c>, and <a, d, b, c>. A permutation can be
+ * specified by an array P, where P[i] represents the location of the element at i in the permutation. For example,
+ * the array <2, 0, 1, 3> represents the permutation that maps the element at location 0 to location 2, the element
+ * at location 1 to location 0, the element at location 2 to location 1, and keep the element at location 3 unchanged.
+ * A permutation can be applied to an array to reorder the array. For example, the permutation <2, 0, 1, 3> applied to
+ * A = <a, b, c, d> yields the array <b, c, a, d>.
+ *
+ * Given an array A of n elements and a permutation P, apply P to A.
+ */
 public class PermuteArray {
     private final Logger logger = LogManager.getLogger(PermuteArray.class);
 
@@ -38,6 +49,11 @@ public class PermuteArray {
         }
     }
 
+    /**
+     * If we cannot use the sign bit, we can allocate an array of n Booleans indicating whether the element at index i
+     * has been processed. Alternatively, we can avoid using O(n) additional storage, by going from left-to-right and
+     * applying the cycle only if the current position is the leftmost position in the cycle.
+     */
     public void applyPermutation2(final List<Integer> perm, final List<String> A) {
         LogUtils.logSeparator(logger);
         logger.debug("perm: {}, A: {}", perm, A);
